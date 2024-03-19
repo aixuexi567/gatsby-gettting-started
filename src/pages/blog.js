4efd29fba05001c3a4b1e4eab5bd753272652912
爱学兮 <aixuexi567@icloud.com>
2024-03-19 14:57:46 +0800

    Update the blog page query to use the allMdx field instead of allFile

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,8 +6,12 @@ const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
+        {data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))}
       </ul>
     </Layout>
@@ -15,9 +19,14 @@ const BlogPage = ({ data }) => {
 };
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
